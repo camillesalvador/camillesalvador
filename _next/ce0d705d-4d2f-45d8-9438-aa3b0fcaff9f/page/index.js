6d35bb4879b8c283a417a0f835b8c8270e679d59
Camille Salvador <camille.salvador@outlook.com>
2017-11-01 15:24:46 -0600

    rebuild

---
+++ _next/ce0d705d-4d2f-45d8-9438-aa3b0fcaff9f/page/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/', function() {
+            var comp = module.exports=webpackJsonp([5],{213:function(e,t,l){e.exports=l(214)},214:function(e,t,l){"use strict";function a(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var n=l(1),r=a(n),u=l(70),d=a(u),c=l(71),o=a(c);t.default=function(){return r.default.createElement(d.default,null,r.default.createElement("style",{dangerouslySetInnerHTML:{__html:o.default}}),r.default.createElement("div",{className:"hero-head zoomIn"},r.default.createElement("div",{className:"hero-img"}),r.default.createElement("div",{className:"description"},r.default.createElement("p",null,"a web developer",r.default.createElement("br",null),"and a part-time witcher"))))}}},[213]);
+            return { page: comp.default }
+          })
+        
